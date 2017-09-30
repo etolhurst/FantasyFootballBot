2de694c3ea2f7f7919c50dfe4150e614cb70ecae
etolhurst <ectolhurst@gmail.com>
2017-09-30 14:53:39 -0400

    now looks to names in lowercase

--- fantasyBot.py
+++ fantasyBot.py
@@ -1,7 +1,6 @@
-#python reddit bot
+#python fantasy football reddit bot
 
-# A Reddit bot that posts explanation of xkcd comic strips posted in comments
-# The explanation is extracted from http://explainxkcd.com
+# A Reddit bot that sends a message to my personal reddit account 
 # Created by Ayush Dwivedi (/u/kindw)
 # License: MIT License
 
@@ -15,16 +14,11 @@ import requests
 import bs4
 
 path = '/home/eric/Desktop/python/reddit/commented.txt'
-# Location of file where id's of already visited comments are maintained
-
-#header = '**Explanation of this xkcd:**\n'
-#footer = '\n*---This explanation was extracted from [explainxkcd](http://www.explainxkcd.com) | Bot created by u/kindw | [Source code](https://github.com/aydwi/explainxkcdbot)*'
-# Text to be posted along with comic description
 
 myTeam = ["Aaron Rodgers", "Devonta Freeman", "Ezekiel Elliott", "Jordy Nelson",
           "Michael Crabtree", "Jordan Reed", "Adam Thielen", "Matt Bryant", "Bilal Powell", 
           "Martavis Bryant", "DeSean Jackson", "Adrian Peterson", "Ted Ginn Jr.", "Eddie Lacy", 
-          "Martellus Bennett"]
+          "Martellus Bennett", "Zeke", "Ginn Jr", "Jory", "Rodgers", "Freeman", "Crabtree"]
 
 def authenticate():
     
@@ -33,13 +27,14 @@ def authenticate():
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
 
-def run_explainbot(reddit):
+def run_fantasybot(reddit):
     
-    print("Getting 250 comments...\n")
+    print("Getting recent comments...\n")
     
-    for comment in reddit.subreddit('fantasyfootball').comments(limit = 250):
+    for comment in reddit.subreddit('fantasyfootball').comments(limit = 50):
         for player in range(len(myTeam)):
-            match = re.findall(myTeam[player], comment.body)
+        	#converting comments and players in myTeam to lowercase and comparing the two
+            match = re.findall(myTeam[player].lower(), comment.body.lower())
             if match:
                 print('Player found in comment with comment ID: ' + comment.id)
                 player_url = match[0]
@@ -48,20 +43,18 @@ def run_explainbot(reddit):
                 file_obj_r = open(path,'r')
                         
                 if comment.id not in file_obj_r.read().splitlines():
-                    print('Link is unique...posting explanation\n')
-                    #comment.reply("player found")
-                    msg = 'Comment: ' + comment.body + '\n' + 'Link: ' + comment.permalink(fast=False)
+                    print('Comment is new...sending comment\n')
+                    msg = 'Comment: ' + comment.body + "\n" + 'Link: ' + comment.permalink(fast=False)
                     reddit.redditor('etolhurst').message('Found Comment About ' + myTeam[player], msg)
 
                     file_obj_r.close()
-
                     file_obj_w = open(path,'a+')
                     file_obj_w.write(comment.id + '\n')
                     file_obj_w.close()
                 else:
                     print('Already visited link...no message needed\n')
-            
-        time.sleep(10)
+        print('Wating 1 second')    
+        time.sleep(1)
 
     print('Waiting 1 minute...\n')
     time.sleep(60)
@@ -70,7 +63,7 @@ def run_explainbot(reddit):
 def main():
     reddit = authenticate()
     while True:
-        run_explainbot(reddit)
+        run_fantasybot(reddit)
 
 
 if __name__ == '__main__':
